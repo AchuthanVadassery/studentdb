a45a85514f95aa63df8c1a314794dbdab0cc4799
AchuthanVadassery <achuthan.v@cubettech.com>
2021-10-12 15:43:11 +0530

    third commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('register',[CourseController::class,'registerCourse']);
+Route::post('course_update/{id}',[CourseController::class,'update']);
+Route::delete('course_delete/{id}',[CourseController::class,'delete']);
